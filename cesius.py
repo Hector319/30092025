f5b03968d644fc71449a9707ba0778d75a0cee85
Hector319 <hectordiazmu@outlook.es>
2025-09-30 12:16:14 +0200

    30/09/25

---
+++ cesius.py
@@ -0,0 +1,1 @@
+def(temperature_celsius): 
---
+++ kkk.py
@@ -0,0 +1,9 @@
+mass = float(input("Enter the mass (in kilograms): "))
+velocity = float(input("Enter the velocity (in m/s): "))
+kinetic_energy = 0.5 * mass * velocity**2
+
+mesage = "The kinetic energy is: (kinetic_energy) "
+print(mesage)
+
+
+
